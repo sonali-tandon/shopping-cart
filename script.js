1b4153cc4b3520a1f585972cef5e0ed207fea086
Sonali Tandon <sonali_tandon@outlook.com>
2021-03-29 13:20:27 +0530

    event target as product image and not its div

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-function showandhideimg(p,q,r){
-    let x = document.getElementById(p);
-    let y = document.getElementById(q);
-    let z = document.getElementById(r);
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
-        y.style.display = 'none';
-        z.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-        y.style.display = 'block';
-        z.style.display = 'none';
+
+const [cartContentEl] = document.getElementsByClassName('cartcontent');
+cartContentEl.addEventListener('click', (event) => {
+    if(event.target.classList.contains('productimg'))
+    {   
+        const productImgEl = event.target;
+        const cartItemEl = productImgEl.closest('.cartitem');
+        const [expandedImageEl] = cartItemEl.getElementsByClassName('productexpandedimg');
+        const [downArrow] = cartItemEl.getElementsByClassName('fa-angle-down');
+        if(productImgEl.style.display === 'block'){
+            productImgEl.style.display = 'none';
+            expandedImageEl.style.display = 'block';
+            downArrow.style.display = 'block';
+        }
+        else{
+            productImgEl.style.display = 'block';
+            expandedImageEl.style.display = 'none';
+            downArrow.style.display = 'none';
+
+        }
+   
+
     }
-}
+});
 
 
 
