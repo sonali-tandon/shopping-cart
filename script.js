0bac2683bb46a9d7201cc9821f7325dd65a5ec95
Sonali Tandon <sonali_tandon@outlook.com>
2021-03-29 13:46:14 +0530

    image click works, arrow click not working

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 
 const [cartContentEl] = document.getElementsByClassName('cartcontent');
 cartContentEl.addEventListener('click', (event) => {
-    if(event.target.classList.contains('productimg'))
+    console.log(event.target.classList);
+    if(event.target.classList.contains('cartitemimg'))
     {   
         const productImgEl = event.target;
         const cartItemEl = productImgEl.closest('.cartitem');
         const [expandedImageEl] = cartItemEl.getElementsByClassName('productexpandedimg');
         const [downArrow] = cartItemEl.getElementsByClassName('fa-angle-down');
-        if(productImgEl.style.display === 'block'){
+        if(productImgEl.style.display === 'none'){
+            productImgEl.style.display = 'block';
+            expandedImageEl.style.display = 'none';
+            downArrow.style.display = 'none';
+        }
+        else{
             productImgEl.style.display = 'none';
             expandedImageEl.style.display = 'block';
             downArrow.style.display = 'block';
         }
-        else{
-            productImgEl.style.display = 'block';
-            expandedImageEl.style.display = 'none';
-            downArrow.style.display = 'none';
-
-        }
-   
 
     }
 });
